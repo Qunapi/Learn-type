0b74caa6c7ce7d66788f704977f833ddca34a34c
Andrew Yushkevich <qunapi@mail.ru>
2020-01-02 16:20:31 +0300

    feat: navbar & css swap to material-ui/styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from 'assets/logo.svg';
-import 'App.css';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { Nav } from 'components/nav';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { lime, grey } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: lime['A700'] },
+    secondary: { main: grey[50] },
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <CssBaseline>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'>
-            Learn React
-          </a>
+          <Nav></Nav>
         </header>
-      </CssBaseline>
-    </div>
+      </div>
+    </ThemeProvider>
   );
 };
 
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import {
+  Toolbar,
+  makeStyles,
+  createStyles,
+  Theme,
+  IconButton,
+} from '@material-ui/core';
 
-const Nav = () => {
-  return <div></div>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    toolbar: {
+      justifyContent: 'center',
+    },
+    nav: {
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      width: `100%`,
+      [theme.breakpoints.up('md')]: {
+        width: theme.breakpoints.values.md,
+      },
+    },
+    button: {
+      color: theme.palette.text.primary,
+    },
+  }),
+);
+
+export const Nav = () => {
+  const css = useStyles();
+  return (
+    <AppBar>
+      <Toolbar className={css.toolbar}>
+        <nav className={css.nav}>
+          <IconButton aria-label='profile' className={css.button}>
+            <AccountBoxIcon />
+          </IconButton>
+        </nav>
+      </Toolbar>
+    </AppBar>
+  );
 };
